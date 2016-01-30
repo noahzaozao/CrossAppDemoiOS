df4ece65c4dd6b55cce1206378be73beaa3d110d
Noah <234082230@qq.com>
2016-01-30 11:10:45 +0800

    update

--- CrossAppDemoiOS/ViewController.h
+++ CrossAppDemoiOS/ViewController.h
@@ -8,8 +8,27 @@
 
 #import <UIKit/UIKit.h>
 
+@class DFPInterstitial;
+@class GADRequest;
+
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UIButton *moreBtn;
+@property (weak, nonatomic) IBOutlet UILabel *statusLabel;
+
+- (void)requestAd;
+
+/// The interstitial ad.
+@property(nonatomic, strong) DFPInterstitial *interstitial;
+
+/// Makes an ad request. Additional targeting options can be supplied with a request object.
+- (void)loadRequest:(GADRequest *)request;
+
+/// Returns YES if the interstitial is ready to be displayed.
+- (BOOL)isReady;
+
+/// Shows the interstitial ad.
+- (void)show;
 
 @end
 
